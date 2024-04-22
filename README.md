e976edbc295498cdaaf3b268e0189dd0105da9c6
Mahmoud Shakaa <mahmoudshakaa@Mahmouds-MacBook-Pro.local>
2024-04-22 19:58:24 +0300

    init shadcn ui lib

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## TODO
 
+- [x] setup deployement to vercel
 - [x] Setup database
+- [x] init shadcn ui lib
+- [ ] scafle basic UI
+- [ ] Create DB schema
 - [ ] Create a page to show the prefreed path to start learning JS
 - [ ] Implement/design for each section in the path the best way to learn it
 - [ ] Add quizes for each section
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function HomePage() {
   const posts = await db.query.posts.findMany();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center text-white">
       (Just javascript website) in progress
       {posts.map((post) => (
         <div key={post.id}> {post.name}</div>
